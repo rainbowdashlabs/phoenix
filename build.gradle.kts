05d0a4ba1943c72a88aef5ed8e735af1c6e3fa28
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2026-03-30 23:02:36 +0200

    add dependencies and ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,39 @@
 plugins {
-    id("java")
+    application
+    id("com.gradleup.shadow") version "9.2.2"
 }
 
+application.mainClass = "dev.chojo.Main"
 group = "dev.chojo"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven("https://central.sonatype.com/repository/maven-snapshots/")
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.jda) {
+        exclude(module = "opus-java")
+    }
+    implementation(libs.jdacommands)
+
+    implementation(libs.hikari)
+    implementation(libs.postgres)
+    implementation(libs.bundles.sadu)
+
+    implementation(libs.bundles.logback)
+    implementation(libs.slf4j)
+
+    implementation(libs.jspecify)
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.isIncremental = true
+    sourceCompatibility = "25"
+}
+
+tasks.withType<JavaExec>() {
+    jvmArgs("--sun-misc-unsafe-memory-access=allow", "--enable-native-access=ALL-UNNAMED")
 }
